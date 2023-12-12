36c5bab06f3f0b9bebd4568fab6f0a5908e38961
budescode <gospeltruth18@gmail.com>
2023-12-12 10:51:46 +0100

    media and static files added

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static_cdn')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media_cdn/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media_cdn')

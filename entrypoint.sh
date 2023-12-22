30e954e1f0172984f7c1ce2d611e08a7615260c7
budescode <gospeltruth18@gmail.com>
2023-12-22 10:15:37 +0100

    gunicorn added

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
-python manage.py runserver 0.0.0.0:8000
+gunicorn -b 0.0.0.0:8000 project.wsgi --timeout 200 --workers=5 

18c8b0a1c12c52905a83c00f31c6cd64be0f8e67
budescode <gospeltruth18@gmail.com>
2024-02-06 09:10:17 +0100

    certbot domain added to .nginx file

--- index/views.py
+++ index/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from index.models import Book
+
 def indexView(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        author = request.POST.get('author')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+        Book.objects.create(name=name, author=author, description=description, image=image)
+    qs = Book.objects.all() 
+    context = {'qs':qs}
+    return render(request, 'index.html', context)
